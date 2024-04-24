232b15d212e98750c436c6a84a90814bda25a442
augusstt06 <augusstt06@gmail.com>
2024-04-24 14:56:51 +0900

    fix: react template

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-
 import { useEffect, useState } from 'react';
 
 import reactLogo from '@/assets/react.svg';
 
 import viteLogo from '../public/vite.svg';
-
-import './App.css';
+import '@/styles/App.css';
 
 function App() {
   const [count, setCount] = useState(0);
