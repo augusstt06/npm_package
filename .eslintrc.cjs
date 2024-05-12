54d28c2e1a70e184b7200a9c5d4b8dc328cde2b1
augusstt06 <augusstt06@gmail.com>
2024-05-12 18:24:09 +0900

    add generateAccessToken

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,26 +2,24 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react-hooks/recommended",
-    "plugin:import/recommended",
-    "plugin:import/errors",
-    "plugin:import/warnings",
-    "prettier",
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:import/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'prettier',
   ],
-  ignorePatterns: ["dist", ".eslintrc.cjs"],
-  parser: "@typescript-eslint/parser",
-  plugins: ["react-refresh", "import"],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh', 'import'],
   rules: {
-    "react-refresh/only-export-components": [
-      "warn",
-      { allowConstantExport: true },
-    ],
-    "react/react-in-jsx-scope": "off",
-    'react-refresh/only-export-components':'off',
-    "sort-imports": [
-      "error",
+    '@typescript-eslint/no-explicit-any': 'off',
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'react/react-in-jsx-scope': 'off',
+    'react-refresh/only-export-components': 'off',
+    'sort-imports': [
+      'error',
       {
         ignoreCase: true,
         ignoreDeclarationSort: true,
@@ -29,46 +27,40 @@ module.exports = {
         allowSeparatedGroups: true,
       },
     ],
-    "import/order": [
-      "error",
+    'import/order': [
+      'error',
       {
-        "newlines-between": "always",
-        groups: [
-          ["builtin", "external"],
-          "internal",
-          "parent",
-          "sibling",
-          "index",
-        ],
+        'newlines-between': 'always',
+        groups: [['builtin', 'external'], 'internal', 'parent', 'sibling', 'index'],
         pathGroups: [
           {
-            pattern: "next",
-            group: "builtin",
+            pattern: 'next',
+            group: 'builtin',
           },
           {
-            pattern: "react",
-            group: "builtin",
+            pattern: 'react',
+            group: 'builtin',
           },
           {
-            pattern: "src/**",
-            group: "internal",
+            pattern: 'src/**',
+            group: 'internal',
           },
         ],
-        pathGroupsExcludedImportTypes: ["src/**"],
+        pathGroupsExcludedImportTypes: ['src/**'],
         alphabetize: {
-          order: "asc",
+          order: 'asc',
           caseInsensitive: true,
         },
       },
     ],
   },
-  settings:{
-    "import/resolver": {
-      "typescript": {},
-      "node": {
-        "paths": ["src"],
-        "extensions": [".js", ".jsx", ".ts", ".tsx", ".svg"]
-      }
-    }
+  settings: {
+    'import/resolver': {
+      typescript: {},
+      node: {
+        paths: ['src'],
+        extensions: ['.js', '.jsx', '.ts', '.tsx', '.svg'],
+      },
+    },
   },
 };
--- src/lib/utils/auth.ts
+++ src/lib/utils/auth.ts
@@ -4,8 +4,44 @@
  * 2. parameters : api endpoint, web storage 종류
  * 3. refresh 의 경우 어떻게 처리할 것인가?
  */
-const Auth = () => {
-  return;
+import jwt from 'jsonwebtoken';
+
+type AuthOptions = {
+  secretKey: string;
+};
+type Payload = {
+  userId: string;
 };
 
+class Auth {
+  private static readonly defaltOptions: AuthOptions = {
+    secretKey: '',
+  };
+  static async generateAccessToken(
+    endpoint: string,
+    data: any,
+    expirePeriod: string,
+    options?: AuthOptions,
+  ): Promise<string | null> {
+    try {
+      const res = await fetch(endpoint, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      });
+      if (!res.ok) throw new Error('Fail to generate access token');
+
+      const user = await res.json();
+      const { secretKey } = { ...this.defaltOptions, ...options };
+      const payload: Payload = { userId: user.id };
+
+      return jwt.sign(payload, secretKey, { expiresIn: expirePeriod });
+    } catch (err) {
+      console.log(err);
+    }
+    return null;
+  }
+}
 export default Auth;
