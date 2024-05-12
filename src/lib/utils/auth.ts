1ff52eb083115727bdfce50efdbe57298d2f8f26
augusstt06 <augusstt06@gmail.com>
2024-05-12 18:00:48 +0900

    setting for create module

---
+++ src/lib/utils/auth.ts
@@ -0,0 +1,11 @@
+/**
+ * 사용자 인증/인가를 간편하게 할수 있는 npm 패키지
+ * 1. fetch method, jwt -> web storage 에 저장
+ * 2. parameters : api endpoint, web storage 종류
+ * 3. refresh 의 경우 어떻게 처리할 것인가?
+ */
+const Auth = () => {
+  return;
+};
+
+export default Auth;
